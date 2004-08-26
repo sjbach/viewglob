4b912e4af2844d06ba82e6a55680bb83bf3326ec
Stephen Bach <viewglob@sjbach.com>
2004-08-26 15:37:27 +0000

    Updates for release 0.8.3.

--- viewglob/gviewglob/gviewglob.h
+++ viewglob/gviewglob/gviewglob.h
@@ -25,8 +25,8 @@
 #include "file_box.h"
 #include "dlisting.h"
 
-#define GVIEWGLOB_VERSION "0.8.2"
-#define GVIEWGLOB_RELEASE_DATE "August 23, 2004"
+#define GVIEWGLOB_VERSION "0.8.3"
+#define GVIEWGLOB_RELEASE_DATE "August 26, 2004"
 
 G_BEGIN_DECLS
 
