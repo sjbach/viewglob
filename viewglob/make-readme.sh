4eb7bb648056d14e741b53ffc9817ee412e67507
Stephen Bach <viewglob@sjbach.com>
2005-03-10 21:50:57 +0000

    Adding strip-grotty from giFTcurs

--- viewglob/make-readme.sh
+++ viewglob/make-readme.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-# This uses README.ms, which I took from the giFTcurs package.  The output will # need to be hand-modified a bit to get rid of page numbers and extra spacing.
+# This uses strip-grotty, which I took from the giFTcurs package:
+# http://www.nongnu.org/giftcurs/
 
-groff -Tlatin1 -P-cub -ms README.ms
+groff -Tlatin1 -P-cub -ms README.ms | perl ./strip-grotty
 
