44edf2c82965b7d6c41d78047f769799bfef95bd
Stephen Bach <viewglob@sjbach.com>
2005-05-31 18:15:02 +0000

    * README, README.ms: documentation updates * make-readme.sh: output ascii * strip-grotty: don't fix page breaks automatically * configure.ac, vgd/Makefile.am, vgseer/Makefile.am:   Updates for release 2.0.2.

--- viewglob/make-readme.sh
+++ viewglob/make-readme.sh
@@ -2,5 +2,7 @@
 # This uses strip-grotty, which I took from the giFTcurs package:
 # http://www.nongnu.org/giftcurs/
 
-groff -Tlatin1 -P-cub -ms README.ms | perl ./strip-grotty
+#groff -Tlatin1 -P-cub -ms README.ms #| perl ./strip-grotty
+#groff -Tutf8 -P-cub -ms README.ms #| perl ./strip-grotty
+groff -Tascii -P-cub -ms README.ms | perl ./strip-grotty
 
